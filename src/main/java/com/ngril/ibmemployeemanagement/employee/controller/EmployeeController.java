ed99b11e3ea2c6ae19572c4ac7120ff52667a437
ngril <niko.gril@decode.agency>
2022-07-08 00:14:58 +0200

    Added minimal logging to endpoints

--- src/main/java/com/ngril/ibmemployeemanagement/employee/controller/EmployeeController.java
+++ src/main/java/com/ngril/ibmemployeemanagement/employee/controller/EmployeeController.java
@@ -1,16 +1,18 @@
 package com.ngril.ibmemployeemanagement.employee.controller;
 
-import com.ngril.ibmemployeemanagement.employee.service.EmployeeService;
 import com.ngril.ibmemployeemanagement.employee.model.EmployeeDTO;
 import com.ngril.ibmemployeemanagement.employee.model.EmployeeForm;
+import com.ngril.ibmemployeemanagement.employee.service.EmployeeService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/employees")
@@ -20,11 +22,13 @@ public class EmployeeController {
 
   @GetMapping
   List<EmployeeDTO> findAllEmployees() {
+    log.info("Find all employees...");
     return employeeService.findAllEmployees();
   }
 
   @GetMapping("/{id}")
   ResponseEntity<EmployeeDTO> findEmployeeById(@PathVariable final Long id) {
+    log.info("Find employee with id: [%d]...".formatted(id));
     return employeeService
         .findEmployeeById(id)
         .map(employeeDTO -> ResponseEntity.status(HttpStatus.OK).body(employeeDTO))
@@ -33,6 +37,7 @@ public class EmployeeController {
 
   @PostMapping
   ResponseEntity<EmployeeDTO> addEmployee(@Valid @RequestBody final EmployeeForm form) {
+    log.info("Add employee: %s".formatted(form.toString()));
     return employeeService
         .addEmployee(form)
         .map(employeeDTO -> ResponseEntity.status(HttpStatus.CREATED).body(employeeDTO))
@@ -42,6 +47,7 @@ public class EmployeeController {
   @PutMapping("/{id}")
   ResponseEntity<EmployeeDTO> updateEmployee(
       @PathVariable final Long id, @Valid @RequestBody final EmployeeForm form) {
+    log.info("Update employee id [%d] with the following values: %s".formatted(id, form.toString()));
     return employeeService
         .updateEmployee(id, form)
         .map(employeeDTO -> ResponseEntity.status(HttpStatus.OK).body(employeeDTO))
